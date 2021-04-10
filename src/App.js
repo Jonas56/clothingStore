20e56ec9d772aa75b1e0ca6225b4f42857fe2764
Jonas56 <younescasio02@gmail.com>
2021-04-10 17:29:33 +0100

    Created app with create react app and changing files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Homepage from './homepage.component'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <Homepage />
+  }
+}
+
+export default App;
